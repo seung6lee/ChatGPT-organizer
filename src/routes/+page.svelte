0adca14be0697623b0565e4e868bf08bbb61018e
seung6lee <sj@seung6lee.com>
2024-08-10 17:30:09 +0900

    add skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<main>
+    <nav>
+        
+    </nav>
+
+    <section>
+        <header>
+
+        </header>
+
+        <article>
+
+        </article>
+    </section>
+</main>
+
+<style>
+    main {
+        width: 250px;
+        height: 100vh;
+
+        display: flex;
+        flex-direction: row;
+    }
+
+    nav {
+        width: 200px;
+        height: 100vh;
+        background-color: #ECCA9C;
+    }
+
+    section {
+        width: calc(100vw - 200px);
+        height: 100vh;
+        background-color: #E8EFCF;
+    }
+</style>
